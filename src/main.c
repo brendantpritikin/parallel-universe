083fd4bd75d0a9a8db8a7fa579ffc19852c89561
BrendanSeniorThesis <brendanseniorthesis@BrendanSeniorThesiss-MacBook-Pro.local>
2022-02-10 23:34:05 -0500

    Added source code folder to git file management.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <math.h> // include math library
+#include <stdio.h> // inlcude standard IO library
+#include <stdlib.h> //include standard library
+#include <time.h> // include time library.
+
+int main(int argc, char *argv[]){
+    
+
+printf("Program set-up completed. Loaded: \n1. math.h\n2. stdio.h\n3. stdlib.h\n4. time.h\n");
+
+}

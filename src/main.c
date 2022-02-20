1f675dff1d07e67deea763c7536c6f5442d85be5
BrendanSeniorThesis <brendanseniorthesis@BrendanSeniorThesiss-MacBook-Pro.local>
2022-02-20 18:14:48 -0500

    added save_all_node_temps(), temperature_storage_array(), array/limits for saving temp data, temp alert loop, and memory freeing for node_temp_record 2D array. Also refactored old testing code.

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@
 /* Author: Brendan T. Pritikin */
 /* Thesis Advisor: Prof. John Rieffel */
 /* Institution: Union College, Schenectady, NY */
-/* Last Modified: February 19, 2022 */
+/* Last Modified: February 20, 2022 */
 
 
 /**
@@ -54,11 +54,11 @@ double square(double randNum1, double randNum2)
 
 
 /**
- * @brief gets current core/process temp data from Raspberry Pi Nodes.
+ * @brief gets current CPU temp data from a single Raspberry Pi Node.
  * 
- * @return double current temperature from each node, core-by-core.
+ * @return double current CPU temp for a single node.
  */
-double getCoreTemps()
+double get_CPU_Temp()
 {
     FILE *temperatureRecord; //open Temp file from Pi.
     double currentTemperature; //core temperature data.
@@ -68,11 +68,37 @@ double getCoreTemps()
     currentTemperature = (((currentTemperature / 1000) * 9) / 5); // convert from mili-degrees to Fahrenheit.
 
     return currentTemperature;
-
 }
 
+/**
+ * @brief get temperature of each node's CPU, add to array.
+ * 
+ * @param number_of_cores_total 
+ *  
+ */
+void save_all_node_temps(int number_of_cores_total, double** node_temp_record_array, int next_save_index)
+{
+    for(int core = 0; core < (number_of_cores_total-3); core+=4)
+    {
+        double currentTemperature = get_CPU_Temp();
+        printf("Node %d CPU temp: %f\n", (core/4), currentTemperature);
+        node_temp_record_array[core][next_save_index] = currentTemperature;
+        printf("Temp saved.------\n");
+    }
+}
 
-
+/**
+ * @brief create an array to store node CPU temp recordings.
+ * 
+ * @param num_cores_total total cores, to use for sizing to # of nodes.
+ * @param number_of_temp_recordings total recordings to capture for program run.
+ * @return double* array to store data in.
+ */
+double* temperature_storage_array(int num_cores_total, int number_of_temp_recordings)
+{
+    double *temp_array = (double *) malloc((num_cores_total/4) * number_of_temp_recordings * sizeof(int)); //holds each node's temps in a 2D array.
+    return temp_array; // this should return a pointer to the beginning of the array.
+}
 
 
 
@@ -83,10 +109,11 @@ double randNum1; //for squaring fcn.
 double randNum2; //for squaring fcn.
 // double squareResult; //stores squared value to run-up processor core temps via arbitrary work.
 int introMessageDisplayed = 0; //allows display of informational message upon mpirun.
-
-
-
-
+int num_of_temp_recordings = 1000; //number of temperature records to store for each node.
+int current_recording_iteration = 0; //holds current iteration count of temp recording.
+int number_of_cores_outside_MPI_declr = 0;
+double* node_temp_record = temperature_storage_array(number_of_cores_outside_MPI_declr, num_of_temp_recordings);
+const double TEMPERATURE_THRESHOLD = 70.0; //maximum °F setting for CPUs, i.e. temperature limit.
 
 
 // initialize MPI
@@ -126,6 +153,13 @@ printf("\n\nhello from the computer named %s, core %d out of %d.\n\n",
 
 //END MPI TESTING.
 
+//#FIXME - ADD MPI collective-communication for increasing efficiency in calculations...
+//#FIXME - perhaps have an array of values from 1 to number_of_cores, and instruct each core to square the value in
+//#FIXME - its individual node index until 1,000 operations have occurred?
+
+
+number_of_cores_outside_MPI_declr = number_of_cores; //save non-MPI core count. Will be re-initialized with each MPI run. Minimal performance hit.
+
 
 if(introMessageDisplayed == 0 && core_number == 0){
     printf("-----------------------------------------\n");
@@ -143,23 +177,56 @@ if(introMessageDisplayed == 0 && core_number == 0){
 
 printf("\n------------------------------------------\n");
 
+//#FIXME - OLD CODE FOR TEMPERATURE-PULLING. WILL DELETE AFTER TESTING NEW CODE.
+
 /**
  * @brief display core temp once in output.
  * 
  */
-if(core_number == 0)
+if(core_number == 0) //node 0,core1 i.e. first core in first CPU.
 {
-    printf("Current core temperatures are: \n\n");
-    currentTemperature = getCoreTemps();
+    printf("OLD hard-coding FOR TESTING = node 0 CPU temp: \n\n");
+    currentTemperature = get_CPU_Temp();
     printf("%f", currentTemperature);
-
     printf("\n------------------------------------------\n");
+}
 
+if(core_number == 4) //node 1
+{
+    printf("OLD hard-coding FOR TESTING = node 1 CPU temp: \n\n");
+    currentTemperature = get_CPU_Temp();
+    printf("%f", currentTemperature);
+    printf("\n------------------------------------------\n");
+}
+
+if(core_number == 8) // node 2
+{
+    printf("OLD hard-coding FOR TESTING = node 2 CPU temp: \n\n");
+    currentTemperature = get_CPU_Temp();
+    printf("%f", currentTemperature);
+    printf("\n------------------------------------------\n");
 }
 
 
+/**
+ * @brief save currrent CPU temperatures from all connected nodes.
+ * 
+ */
+if(current_recording_iteration < num_of_temp_recordings)
+{
+    save_all_node_temps(number_of_cores, &node_temp_record, current_recording_iteration);
+}
 
-// #FIXME if currentTemperature is too high, delay cycle. Else, run someting like an MPI_scatter/gather, etc.)
+for(int core = 0; core < number_of_cores-3; core+=4)
+{
+    if(node_temp_record[core][current_recording_iteration] > TEMPERATURE_THRESHOLD)
+    {
+        printf("oh no! Too hot to handle!"); // pause the program for a few seconds. #FIXME need to set this up.
+    }
+} 
+
+
+//#FIXME: OLD CODE call/response from cores. WILL DELETE AFTER TESTING NEW CODE.
 
 /**
 
@@ -184,6 +251,8 @@ if(core_number == 3)
 }
 
 */
+
+free(node_temp_record);
 
 MPI_Finalize(); //Finalize MPI environment.
 return 0;
